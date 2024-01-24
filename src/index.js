df6b567f7a87943144d8a6cea61071912b4149c6
Carlos Pinto <pintocar83@gmail.com>
2024-01-23 20:41:51 -0400

    Initial commit PHP Desktop App

---
+++ src/index.js
@@ -0,0 +1,69 @@
+/*
+1)Run command
+npm start
+
+2)Generate package binary in out/
+npm run package
+*/
+
+const { app, BrowserWindow } = require('electron');
+const { spawn } = require('node:child_process');
+
+const host = "127.0.0.1:3535";
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const php = spawn('php/php.exe', ['-S', host, '-t', "www/public"], { stdio: 'ignore' });
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    icon: __dirname + '/icon.ico',
+    width: 1000,
+    height: 800,
+    minWidth: 1000,
+    minHeight: 800,
+  });
+
+
+  mainWindow.menuBarVisible = false;
+  mainWindow.maximize();
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.webContents.setZoomFactor(0.90);
+  });
+
+  mainWindow.loadURL('http://'+host+'/');
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+    php.kill();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
